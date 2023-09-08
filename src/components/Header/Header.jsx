c9652959e5e850a791456b1fa5780efadffa4e28
Dwi M Nurafli <dwirafli13@gmail.com>
2023-09-08 23:24:28 +0700

    styling navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,19 +14,45 @@ const Header = ({ handleChanceSearch, getData }) => {
 
   return (
     <>
-      <div className="container">
-        <nav className="navbar">
+      <nav className="navbar navbar-expand-sm bg-body-tertiary">
+        <div className="container-fluid">
           <Link to={"/menus"}>
-            <img src={logo} />
+            <img src={logo} className="navbar-brand" />
           </Link>
-          <div className="d-flex">
-            <input className="form-control me-2" placeholder="Search" onChange={handleChanceSearch} />
-          </div>
-          <button onClick={handleLogout} className="btn btn-outline-danger">
-            Logout
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarsExample03"
+            aria-controls="navbarsExample03"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
           </button>
-        </nav>
-      </div>
+          <div className="collapse navbar-collapse" id="navbarsExample03">
+            <ul className="navbar-nav me-auto mb-2 mb-sm-0">
+              <li className="nav-item">
+                <Link to={"/menus"} className="nav-link fw-bold" aria-current="page">
+                  Home
+                </Link>
+              </li>
+            </ul>
+            <div className="d-flex">
+              <input
+                className="form-control"
+                type="search"
+                placeholder="Search"
+                onChange={handleChanceSearch}
+                role="search"
+              />
+              <button onClick={handleLogout} className="btn btn-danger ms-3">
+                Logout
+              </button>
+            </div>
+          </div>
+        </div>
+      </nav>
     </>
   );
 };
